c5f885630065bff5bfedae7510995145ca2cd26f
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-06-13 15:58:49 +0200

    Worked on es-01

--- es-01/src/main/scala/Main.scala
+++ es-01/src/main/scala/Main.scala
@@ -1,25 +1,71 @@
-import akka.actor.typed.{ActorSystem, Behavior}
+import ReportAggregator.Command.AddStatistic
+import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
 import akka.actor.typed.scaladsl.Behaviors
 
-object ExampleActor:
+import java.nio.file.{Files, Path}
+import scala.concurrent.Future
+
+case class Statistic(path: Path, size: Long)
+
+object ReportAggregator:
   enum Command:
-    case Hello(name: String)
-    case Goodbye
+    case AddStatistic(statistic: Statistic)
+    case Complete
 
   def apply(): Behavior[Command] =
-    Behaviors.receiveMessage {
-      case Command.Hello(name) =>
-        println(s"Hello, $name!")
-        Behaviors.same
-      case Command.Goodbye =>
-        println("Goodbye!")
-        Behaviors.stopped
+    import Command._
+    Behaviors.setup { context =>
+      Behaviors.receiveMessage {
+        case AddStatistic(statistic) =>
+          // TODO: add statistic
+          Behaviors.same
+        case Complete =>
+          Behaviors.stopped
+      }
+    }
+
+object FileScanner:
+  enum Command:
+    case Scan(path: Path, replyTo: ActorRef[ReportAggregator.Command])
+
+  def apply(): Behavior[Command] =
+    import Command._
+    Behaviors.setup { context =>
+      Behaviors.receiveMessage {
+        case Scan(path, replyTo) =>
+          // TODO: scan file
+          Behaviors.same
+      }
+    }
+
+object FolderScanner:
+  enum Command:
+    case Scan(path: Path, replyTo: ActorRef[FileScanner.Command])
+
+  def apply(): Behavior[Command] =
+    import Command._
+    Behaviors.setup { context =>
+      Behaviors.receiveMessage {
+        case Scan(path, replyTo) =>
+          // TODO: scan folder
+          Behaviors.same
+      }
+    }
+
+object App:
+  enum Command:
+    case Start(path: Path)
+
+  def apply(): Behavior[Nothing] =
+    Behaviors.setup { context =>
+      Behaviors.receiveMessage {
+        case _ => Behaviors.same
+      }
     }
 
 @main def main(): Unit =
-  val system = ActorSystem(ExampleActor(), "example")
-  system ! ExampleActor.Command.Hello("Alice")
-  system ! ExampleActor.Command.Goodbye
+  val system = ActorSystem(App(), "example")
 
 
 
